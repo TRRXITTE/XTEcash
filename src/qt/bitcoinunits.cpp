7628b2b560f37abad3404a4b85e0226294f88b47
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2024-08-20 11:11:59 +0200

    add coinbase tx

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -31,10 +31,10 @@ QList<BitcoinUnit> BitcoinUnits::availableUnits()
 QString BitcoinUnits::longName(Unit unit)
 {
     switch (unit) {
-    case Unit::BTC: return QString("BTC");
-    case Unit::mBTC: return QString("mBTC");
-    case Unit::uBTC: return QString::fromUtf8("µBTC (bits)");
-    case Unit::SAT: return QString("Satoshi (sat)");
+    case Unit::BTC: return QString("XTEcash");
+    case Unit::mBTC: return QString("mXTEcash");
+    case Unit::uBTC: return QString::fromUtf8("µXTEcash(bits)");
+    case Unit::SAT: return QString("sat's");
     } // no default case, so the compiler can warn about missing cases
     assert(false);
 }
@@ -53,10 +53,10 @@ QString BitcoinUnits::shortName(Unit unit)
 QString BitcoinUnits::description(Unit unit)
 {
     switch (unit) {
-    case Unit::BTC: return QString("Bitcoins");
-    case Unit::mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case Unit::uBTC: return QString("Micro-Bitcoins (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case Unit::SAT: return QString("Satoshi (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case Unit::BTC: return QString("XTEcash");
+    case Unit::mBTC: return QString("Milli-XTEcash (1 / 1" THIN_SP_UTF8 "000)");
+    case Unit::uBTC: return QString("Micro-XTEcash (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case Unit::SAT: return QString("sat's (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     } // no default case, so the compiler can warn about missing cases
     assert(false);
 }
