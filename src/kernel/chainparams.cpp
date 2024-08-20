b416e575b68d1da8a5548b7ebe90a42e6841bb85
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2024-08-20 21:36:00 +0200

    add dns seed

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -130,8 +130,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("eu.trrxitte.com."); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("eu-west.trrxitte.com."); // Matt Corallo, only supports x9
+        vSeeds.emplace_back("dns.trrxitte.com."); // Pieter Wuille, only supports x1, x5, x9, and xd
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
